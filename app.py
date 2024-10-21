6be6d494713b3e314c7483969d2daafefdbfdfa0
Hk574 <hk574@njit.edu>
2024-10-21 08:29:00 +0530

    added cal methods

--- app.py
+++ app.py
@@ -30,9 +30,47 @@ class App:
             try:
                 cmd_input = input(">>> ").strip()
                 if cmd_input.lower() == 'exit':
-                    logging.info("Application exit.")
-                    sys.exit(0)  # Use sys.exit(0) for a clean exit, indicating success.
+                    logging.info("Exiting the calculator.")
+                    print("Exiting the calculator.")
+                    break
 
+                if cmd_input.lower() == 'history':
+                    print("Calculation History:")
+                    print(calculator.show_history())
+                    break
+                
+                if cmd_input.lower() == 'load_history':
+                    logging.info("Loading history.")
+                    print(calculator.load_history())
+                    continue
+
+                elif cmd_input.lower() == "save_history":
+                    logging.info("Saving history.")
+                    print(calculator.save_history())
+                    continue
+
+                elif cmd_input.lower() == "clear_history":
+                 logging.info("Clearing history.")
+                 print(calculator.clear_history())
+                 continue
+
+                elif cmd_input.startswith("delete_history_record"):
+                    cmd_parts = cmd_input.split()
+
+                    if len(cmd_parts) == 2 and cmd_parts[1].isdigit():
+                        index = int(cmd_parts[1])
+                        logging.info(f"Deleting history record at index: {index}")
+                        print(calculator.delete_history_record(index))
+                    else:
+                        logging.warning("Invalid index provided for delete_history_record.")
+                        print("Error: Please provide a valid index to delete.")
+                    continue
+
+ 
+
+                elif cmd_input.lower() == 'menu':
+                    print("Available commands:")
+                    print(commandHandler.list_plugins())
                 # Split the command and its arguments
                 cmd_parts = cmd_input.split()
                 if len(cmd_parts) == 0:
